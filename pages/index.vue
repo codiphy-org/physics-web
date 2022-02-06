5a915bc4c89bcfe584b0095d289ca6133312e221
Ashwin Kamath <ashwinkn@hotmail.com>
2022-02-06 17:20:31 -0600

    Removed more branding

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,19 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <logo />
+        <h1>Welcome to Physics web</h1>
       </div>
       <v-card>
-        <v-card-title class="headline"> Welcome to Physics web </v-card-title>
-        <v-card-text> </v-card-text>
+        <v-card-title class="headline">Introductory text</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
         <v-card-actions>
           <v-spacer />
         </v-card-actions>
@@ -16,11 +24,7 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
 export default {
-  components: {
-    Logo,
-  },
+  components: {},
 };
 </script>
